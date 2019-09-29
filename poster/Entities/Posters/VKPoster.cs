5e7d1dba29b2ce283f6aa59506e161d296ca5db3
Ewgraf <ewgraf@users.noreply.github.com>
2019-09-29 10:45:21 +0300

    +

--- poster/Entities/Posters/VKPoster.cs
+++ poster/Entities/Posters/VKPoster.cs
@@ -6,13 +6,21 @@ using Telegram.Bot.Types;
 
 namespace poster.Entities.Posters {
 	public class VkPoster : Poster {
-		public VkPoster() {
+		private readonly string _appId;
+		private readonly string _login;
+		private readonly string _password;
+
+		public VkPoster(string appId, string login, string password) {
+			_appId = appId;
+			_login = login;
+			_password = password;
 		}
 
 		public override async Task Post(Post post) {
 			try {
+
 			} catch (Exception ex) {
-				Console.WriteLine($"telegram> {ex.ToString()}");
+				Console.WriteLine($"vk> {ex.ToString()}");
 			}
 		}
 	}
