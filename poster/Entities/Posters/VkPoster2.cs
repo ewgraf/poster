70bf3af5ac868577ff8e337f455ab9d2f164bc2b
Ewgraf <ewgraf@users.noreply.github.com>
2019-09-29 11:00:20 +0300

    +

--- poster/Entities/Posters/VkPoster2.cs
+++ poster/Entities/Posters/VkPoster2.cs
@@ -6,12 +6,12 @@ using VkNet.Model.Attachments;
 using VkNet.Model.RequestParams;
 
 namespace poster.Entities.Posters {
-	public class VkPoster : Poster {
+	public class VkPoster2 : Poster {
 		private readonly string _appId;
 		private readonly string _login;
 		private readonly string _password;
 
-		public VkPoster(string appId, string login, string password) {
+		public VkPoster2(string appId, string login, string password) {
 			_appId = appId;
 			_login = login;
 			_password = password;
