f72ef324ae91e2262ddfa45d38f355fce9512806
Ewgraf <ewgraf@users.noreply.github.com>
2019-09-29 03:34:43 +0300

    +

---
+++ poster/Entities/Posters/VKPoster.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using MihaZupan;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace poster.Entities.Posters {
+	public class VkPoster : Poster {
+		public VkPoster() {
+		}
+
+		public override async Task Post(Post post) {
+			try {
+			} catch (Exception ex) {
+				Console.WriteLine($"telegram> {ex.ToString()}");
+			}
+		}
+	}
+}
